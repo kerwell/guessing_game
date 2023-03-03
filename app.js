ae6d21a0dc255713e79a3ae5d19e2ead53286127
Liam DeWitt <mugzy550@gmail.com>
2023-03-03 01:37:47 -0500

    change losing message from prompt to alert

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 let maximum = parseInt(prompt("Give me a maximum number!"));
 // parseInt to make any string into a number. if its not a number, it return "NaN"
-while (!maximum){
-    maximum = parseInt(prompt("That is not a valid number. Please try again!"));
+while (!maximum) {
+  maximum = parseInt(prompt("That is not a valid number. Please try again!"));
 }
 //we are using a while loop so that so long as this is invalid, we keep asking
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
 
-let guess = parseInt(prompt("Enter your first guess!"))
+let guess = parseInt(prompt("Enter your first guess!"));
 let attempts = 1;
 // attempts start at one because once you made it past the first guess, that's
 //when it starts
 
-while (parseInt(guess) !== targetNum){
-    console.log(`attempt number ${attempts}`)
-    attempts++;
-    if (guess === 'q'){
-        guess = prompt("Okay loser, lmao.");
-        break;
-    }
-    if (guess > targetNum){
-        guess = prompt("Too high! Try again:");
-    } else{
-        guess = prompt("Too low! Try again:");
-        // we are already in a loop of failure. if its not equal, its less than or greater
-        //though can be an 'else if' if you want
-    }
-
+while (parseInt(guess) !== targetNum) {
+  console.log(`attempt number ${attempts}`);
+  attempts++;
+  if (guess === "q") {
+    guess = alert("Okay loser, lmao.");
+    break;
+    //breaks will end the loop
+  }
+  if (guess > targetNum) {
+    guess = prompt("Too high! Try again:");
+  } else {
+    guess = prompt("Too low! Try again:");
+    // we are already in a loop of failure. if its not equal, its less than or greater
+    //though can be an 'else if' if you want
+  }
 }
 //put an if for when they get it right
-alert(`You got it! It took you ${attempts} tries!`)
-console.log(`You got it! It took you ${attempts} tries!`)
+
+if (parseInt(guess) === targetNum) {
+  alert(`You got it! It took you ${attempts} tries!`);
+  console.log(`You got it! It took you ${attempts} tries!`);
+}
